e0050d1f81bbf4b886e1adc97c77684a7cae03df
Alejandro Romero <esojandro@gmail.com>
2022-09-13 16:20:48 -0600

    Test 1 delete method was added

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -2,30 +2,28 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static io.restassured.RestAssured.baseURI;
-
 public class Test2 {
     @Test
     public void loginTest(){ //WIP
 
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("email", "elchobylobi@mail.com");
-        map.put("password", "guardiandegalletas");
-
         String requestBody = RestAssured
                 .given()
+                .log().all()
                 .contentType(ContentType.JSON)
-                .body(map.toString())
+                .body(
+                        "{\n" +
+                                " \"email\": \"elchobylobi@mail.com\",\n" +
+                                " \"password\": \"comegalletaslomito\"\n" +
+                                "}"
+                )
                 .post("https://reqres.in/api/login")
                 .then()
-                //.statusCode(200)
-                .log().all()
+                .statusCode(200)
+                .log()
+                .all()
                 .extract()
                 .asString();
 
-        //System.out.println(requestBody);
+        System.out.println(requestBody);
     }
 }
